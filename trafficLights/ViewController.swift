04105425b3d0348458884284baa7597d8fc636ac
dv.kulikov <dv.kulikov@mail.ru>
2020-10-15 00:28:33 +0300

    frame size wrong constraints

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var startButton: UIButton!
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+        
     }
 
-
+    @IBAction func startButtonPressed() {
+        var colourOfLight = "red"
+        switch colourOfLight {
+        case "red":
+            redLightView.alpha = 1
+            startButton.setTitle("NEXT", for: .normal)
+            colourOfLight = "yellow"
+        case "yellow":
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            colourOfLight = "green"
+        case "green":
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        default:
+            greenLightView.alpha = 0.3
+        }
+        
+    }
+    
+    
 }
 
